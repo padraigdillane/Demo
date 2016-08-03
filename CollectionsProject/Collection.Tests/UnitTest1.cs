f6c1f837d412e1a96ca17491bfcdb0cc430e461f
padraigdillane <padraigdillane@gmail.com>
2016-08-03 12:09:51 +0100

    Added test project

---
+++ CollectionsProject/Collection.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Collection.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
